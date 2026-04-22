d43eb5400c25533fb40c943e89ef0b2f44ef2b80
ssugary <ssugary0@gmail.com>
2026-04-22 20:36:26 -0300

    fix: Correção dos parâmetros da função convolucao() e método de exportar imagem filtrada em novo arquivo

--- scripts/q2.cpp
+++ scripts/q2.cpp
@@ -1,56 +1,25 @@
-#include <cstddef>
+#include <cstdlib>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/core/saturate.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/opencv.hpp>
-#include "glm/glm.hpp"
-#include "glm/matrix.hpp"
 #include <iostream>
 
+const int d = 3; // Dimensão
 
-cv::Mat convolucao(cv::Mat img, char p){
-    int m{3}, n{3};
-    int altura{img.rows}, largura{img.cols};
+cv::Mat convolucao(const cv::Mat& img, const cv::Mat& kernel, const int& d){
+    int altura{img.rows}, largura{img.cols}, pad{d/2};
 
     cv::Mat img_filtrada = cv::Mat(img).clone();
 
-    int pad_alt{m/2}, pad_larg{n/2};
-
-    glm::mat3x3 kernel;
-
-    switch(p){
-        case 'h':
-            kernel = {0.0f, -1.0f, 0.0f, -1.0f, 4.0f, -1.0f, 0.0f, -1.0f, 0.0f};
-            break;
-        case 'l':
-            kernel = {1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f};
-            kernel /= 9;
-            break;
-        default:
-            return img;
-        }
-
-    // kernel = glm::inverse(kernel);
-    cv::Mat img_pad;
-
-    cv::copyMakeBorder(
-    img,               
-    img_pad,           
-    pad_alt,           
-    pad_alt,           
-    pad_larg,          
-    pad_larg,          
-    cv::BORDER_REFLECT 
-    );
-
-    for(int y{0}; y < altura; y++){
-        for(int x{0}; x < largura; x++){
+    for(int y{pad}; y < altura - pad; y++){
+        for(int x{pad}; x < largura - pad; x++){
             float soma{0.0};
 
-            for(int i{0}; i < m; i++){
-                for(int j{0}; j < n; j++){
-                    soma += ((float)img_pad.at<uchar>(y + i, x + j)) * kernel[i][j];
+            for(int i{0}; i < d; i++){
+                for(int j{0}; j < d; j++){
+                    soma += static_cast<float>(img.at<uchar>(y + i - pad, x + j - pad)) * kernel.at<float>(i, j);
                 }
             }
             img_filtrada.at<uchar>(y, x) = cv::saturate_cast<uchar>(soma);
@@ -63,12 +32,37 @@ cv::Mat convolucao(cv::Mat img, char p){
 int main(int argc, char* argv[]) {
    
     cv::Mat img = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
+    cv::Mat kernel = cv::Mat::zeros(d, d, CV_32FC1);
+    switch(*argv[2]){
+        case 'h':
+            kernel = (cv::Mat_<float>(3,3) << 
+                      0.0f, 1.0f, 0.0f, 
+                      1.0f, -4.0f, 1.0f, 
+                      0.0f, 1.0f, 0.0f
+                     );
+            break;
+        case 'l':
+            kernel = (cv::Mat_<float>(3, 3) << 
+                      1.0f, 1.0f, 1.0f,
+                      1.0f, 1.0f, 1.0f, 
+                      1.0f, 1.0f, 1.0f
+                     );
+            kernel /= 9.0f;
+            break;
+        default:
+            std::cerr << "Error: invalid arguments " + std::string(argv[2]) << '\n';
+            return EXIT_FAILURE;
+    }
 
-    cv::Mat img_filtrada = convolucao(img, *argv[2]);
+    cv::Mat img_filtrada = convolucao(img, kernel ,d);
 
-    cv::imshow("Imagem filtrada",img_filtrada);
-    cv::waitKey(0); // Espera você apertar uma tecla
+    bool sucesso = cv::imwrite("resultado_filtrado.png", img_filtrada);
     
-    cv::destroyAllWindows();
+    if (sucesso) {
+        std::cout << "Imagem salva com sucesso como 'resultado_filtrado.png'!\n";
+    } else {
+        std::cerr << "Erro ao salvar a imagem.\n";
+    }
+    
     return 0;
 }
