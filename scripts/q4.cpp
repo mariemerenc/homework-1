a4d27ed1620e3e65a8358e89635df2aae7097c63
ssugary <ssugary0@gmail.com>
2026-04-22 22:04:27 -0300

    refactor: utiliza a função de median_filter apenas com 1 inteiro ao invés de um par de inteiros iguais

--- scripts/q4.cpp
+++ scripts/q4.cpp
@@ -4,36 +4,34 @@
 
 #include <opencv2/opencv.hpp>
 
-cv::Mat median_filter(cv::Mat img, std::pair<int, int> tamanho_janela){
-    int m{tamanho_janela.first}, n{tamanho_janela.second};
+cv::Mat median_filter(cv::Mat img, int tamanho_janela){
     int altura{img.rows}, largura{img.cols};
 
     cv::Mat img_filtrada = cv::Mat(img).clone();
 
-    int pad_alt = m/2;
-    int pad_larg = n/2;
+    int pad = tamanho_janela/2;
 
     cv::Mat img_pad;
 
     cv::copyMakeBorder(
     img,               
     img_pad,           
-    pad_alt,           
-    pad_alt,           
-    pad_larg,          
-    pad_larg,          
+    pad,           
+    pad,           
+    pad,          
+    pad,          
     cv::BORDER_REFLECT 
     );
 
     for(int y{0}; y < altura; ++y){
         for(int x{0}; x < largura; ++x){
-            cv::Rect roi(x, y,n,m);
+            cv::Rect roi(x, y,tamanho_janela,tamanho_janela);
             cv::Mat vizinhanca = img_pad(roi);
             std::vector<uchar> pixels;
-            pixels.reserve(m * n);
+            pixels.reserve(tamanho_janela * tamanho_janela);
 
-            for(int i{0}; i < m; ++i){
-                for(int j{0}; j < n; ++j){
+            for(int i{0}; i < tamanho_janela; ++i){
+                for(int j{0}; j < tamanho_janela; ++j){
                     pixels.push_back(vizinhanca.at<uchar>(i, j));
                 }
             }
@@ -51,7 +49,7 @@ int main(int argc, char* argv[]) {
    
     cv::Mat img = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
 
-    cv::Mat img_filtrada = median_filter(img, {std::stoi(argv[2]), std::stoi(argv[2])});
+    cv::Mat img_filtrada = median_filter(img, std::stoi(argv[2]));
 
     cv::imshow("Imagem filtrada",img_filtrada);
     cv::waitKey(0); // Espera você apertar uma tecla
